2ab2bc8ca82b9ef8ae11abcf832afca6605dbfe0
shubhamprpcoe <shubhamprpcoe@gmail.com>
2022-11-04 21:25:43 +0530

    nodemon added

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-console.log("server side code")
+const express = require('express')
+const app = express()
+
+app.get('/home', (req, res) => {
+  res.send('{durgesh:kadwe}')
+})
+
+app.listen(4000, () => {
+  console.log(`Example app listening on port 4000`)
+})
